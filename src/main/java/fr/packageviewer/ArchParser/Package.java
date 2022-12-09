4bd7f32daeb92dff36d42d1622fc162c550cfda5
Capelier-Marla <marla.capelier@etu.univ-amu.fr>
2022-12-09 20:03:50 +0100

    add Package list into Packages

--- src/main/java/fr/packageviewer/ArchParser/Package.java
+++ src/main/java/fr/packageviewer/ArchParser/Package.java
@@ -9,11 +9,12 @@ public class Package {
     String description;
     List<Package> deps;
     
-    public Package(String name, String version, String repo, String desciption) {
+    public Package(String name, String version, String repo, String desciption, List<Package> deps) {
         this.name = name;
         this.version = version;
         this.repo = repo;
         this.description = desciption;
+        this.deps = deps;
     }
     
 }
