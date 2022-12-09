5bfa9c8f0f9939b37d74f473338d08070c874587
SIMAILA Djalim <djalimS.pro@outlook.fr>
2022-12-09 16:11:43 +0100

    removed arch parser for refactoring

--- src/main/java/fr/packageviewer/ArchParser/ArchParser.java
+++ src/main/java/fr/packageviewer/ArchParser/ArchParser.java
@@ -1,9 +1,14 @@
 package fr.packageviewer.ArchParser;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 public class ArchParser {
     
+    public static Package getPackageTree(String packageName, int deph){
 
-    public Package getPackageTree(String packageName, int deph){
-        
     }
 }
