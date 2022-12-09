c3ab4d3dcf3c096801d267adbb87a1dde3b232f1
SIMAILA Djalim <djalimS.pro@outlook.fr>
2022-12-09 16:21:40 +0100

    added temporary package class

---
+++ src/main/java/fr/packageviewer/FedoraParser/Package.java
@@ -0,0 +1,20 @@
+package fr.packageviewer.FedoraParser;
+
+import java.util.List;
+
+public class Package {
+    String name;
+    String version;
+    String repo;
+    String description;
+    List<Package> deps;
+    
+    public Package(String name, String version, String repo, String desciption) {
+        this.name = name;
+        this.version = version;
+        this.repo = repo;
+        this.description = desciption;
+    }
+    
+    
+}
