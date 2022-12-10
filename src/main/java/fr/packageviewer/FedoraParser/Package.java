37f8e2ea36ce0bf17ee57bdd60177288f222c36f
SIMAILA Djalim <djalimS.pro@outlook.fr>
2022-12-10 13:28:12 +0100

    refractored to respect specifications

--- src/main/java/fr/packageviewer/FedoraParser/Package.java
+++ src/main/java/fr/packageviewer/FedoraParser/Package.java
@@ -2,19 +2,12 @@ package fr.packageviewer.FedoraParser;
 
 import java.util.List;
 
-public class Package {
-    String name;
-    String version;
-    String repo;
-    String description;
+public class Package extends SearchedPackage {
     List<Package> deps;
-    
-    public Package(String name, String version, String repo, String desciption) {
-        this.name = name;
-        this.version = version;
-        this.repo = repo;
-        this.description = desciption;
+
+    public Package(String name, String version, String repo, String description, List<Package> deps) {
+        super(name, version, repo, description);
+        this.deps = deps;
     }
-    
-    
+
 }
