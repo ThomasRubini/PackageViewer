acead35d21fb68086502ae5c666e26f32c990cb7
SIMAILA Djalim <djalimS.pro@outlook.fr>
2022-12-10 13:33:54 +0100

    modified package structure for fedora + added search method

--- src/main/java/fr/packageviewer/FedoraParser/Package.java
+++ src/main/java/fr/packageviewer/FedoraParser/Package.java
@@ -3,10 +3,12 @@ package fr.packageviewer.FedoraParser;
 import java.util.List;
 
 public class Package extends SearchedPackage {
+    String version;
     List<Package> deps;
 
     public Package(String name, String version, String repo, String description, List<Package> deps) {
-        super(name, version, repo, description);
+        super(name, repo, description);
+        this.version = version;
         this.deps = deps;
     }
 
--- src/main/java/fr/packageviewer/FedoraParser/SearchedPackage.java
+++ src/main/java/fr/packageviewer/FedoraParser/SearchedPackage.java
@@ -2,13 +2,11 @@ package fr.packageviewer.FedoraParser;
 
 public class SearchedPackage {
     String name;
-    String version;
     String repo;
     String description;
 
-    public SearchedPackage(String name, String version, String repo, String desciption) {
+    public SearchedPackage(String name, String repo, String desciption) {
         this.name = name;
-        this.version = version;
         this.repo = repo;
         this.description = desciption;
     }
