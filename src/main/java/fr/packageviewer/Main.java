9541ab571823848c2e1a8a212a954654b5385cee
Capelier-Marla <marla.capelier@etu.univ-amu.fr>
2022-12-12 22:46:18 +0100

    added working arguments parser with doc

--- src/main/java/fr/packageviewer/Main.java
+++ src/main/java/fr/packageviewer/Main.java
@@ -1,7 +1,33 @@
 package fr.packageviewer;
 
+import com.beust.jcommander.JCommander;
+
 public class Main {
+
+	/* distribution the user want */
+	private static String distribution;
+
+	/**
+	 * Get the command line argument given by the user, parse it with the parser and store it in the corresponding variable
+	 * @author Capelier-Marla
+	 * @param args the command line arguments given by the user
+	 * @return void
+	 */
+	static void parseArguments(String[] args) {
+		// create JCommander and CommandLineParams objects
+		JCommander jCommander = new JCommander();
+		CommandLineParams params = new CommandLineParams();
+		// add argument required by the params to the JCommander object
+		jCommander.addObject(params);
+		// parse the argument from list of String
+		jCommander.parse(args);
+		// store the argument parsed in the variable
+		distribution = params.distribution;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		// send the command line arguments to the parser
+		parseArguments(args);
+		System.out.println(distribution);
 	}
 }
