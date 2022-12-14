4d2c67c1c247dfaec4e41d8276afc915009e50ae
SIMAILA Djalim <djalimS.pro@outlook.fr>
2022-12-14 15:15:55 +0100

    finished documentation for class Logger

--- src/main/java/fr/packageviewer/LoggerManager.java
+++ src/main/java/fr/packageviewer/LoggerManager.java
@@ -2,15 +2,37 @@ package fr.packageviewer;
 
 import java.util.logging.*;
 
+/**
+ * The LoggerManager class allows for basic debug output management using
+ * Java's default logging class.
+ * 
+ * @author R.Thomas
+ * @version 1.0
+ */
 public class LoggerManager {
-
+	/**
+	 * Default log level, should be INFO
+	 */
 	private static final Level DEFAULT_LOG_LEVEL = Level.INFO;
 
-	public static Logger getLogger(String name){
+	/**
+	 * Static factory for the Logger class
+	 * 
+	 * @param name String, of the logger to create
+	 * @return Logger, a new logger
+	 */
+	public static Logger getLogger(String name) {
 		return getLogger(name, DEFAULT_LOG_LEVEL);
 	}
 
-	public static Logger getLogger(String name, Level level){
+	/**
+	 * Main static factory for the Logger class
+	 *
+	 * @param name  String, name of the logger to create
+	 * @param level Level, the level of severity of the logger
+	 * @return Logger, a new logger
+	 */
+	public static Logger getLogger(String name, Level level) {
 		Logger logger = Logger.getLogger(name);
 		logger.setLevel(level);
 
